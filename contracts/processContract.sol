e2164c028ad6b1c734fb8636513e458f9ca8e267
kali <rafi.khudayar@gmail.com>
2022-06-18 01:51:22 +0300

    Decentrlised Systems

---
+++ contracts/processContract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Unlicensed
+
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract processContract
+{
+
+
+
+
+}
+
+
+// Precess Steps
+
+//  Max 20C Min 10C     Washed
+//  Plastic, Foam, Carton, Paper, Wooden Box             Packing
+//  Chemical componds
+
