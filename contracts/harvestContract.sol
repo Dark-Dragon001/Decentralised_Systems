137e4d5fc7978afbebfa7b35154aaf2aa76e4c19
kali <rafi.khudayar@gmail.com>
2022-06-20 02:52:19 +0300

    Decentrlised Systems

--- contracts/harvestContract.sol
+++ contracts/harvestContract.sol
@@ -10,48 +10,67 @@ contract harvestContract
     string[3] goodsSize = ["Medium", "Large", "Extra-large"];
     string[4] goodsQuality = ["Raw", "Half-ripen", "Ripen", "Full-ripen"];
 
+    // Variables that are supposed to take data form the user.
+    string storeGoods;
+    string storeGoodsSize;
+    string storeGoodsQuality;
 
-    function getGoods() public view returns (string[10] memory, string[3] memory, string[4] memory)
+
+    function getContractConditions() public view returns (string[10] memory, string[3] memory, string[4] memory)
     {
         // View the product arrays.
         return (goods, goodsSize, goodsQuality);
     }
 
+    function setGoods(string memory _var1, string memory _var2, string memory _var3) public
+    {
+        // Takes inputs from the user and sets the value of strings above.
+        storeGoods = _var1;
+        storeGoodsSize = _var2;
+        storeGoodsQuality = _var3;
+    }
 
-    function inputGood(string memory _goods) public view returns (string memory)
+    function getGoods() public view returns(string memory, string memory, string memory)
+    {
+        // Views the values entered by the user
+        return (storeGoods, storeGoodsSize, storeGoodsQuality);
+    }
+
+    function inputGood() public view returns(string memory )
     {
         // Check whether the new product matches the products pre-defined in the goods array.
         for (uint i = 0; i < goods.length; i++)
         {
-            if (keccak256(abi.encodePacked(_goods)) == keccak256(abi.encodePacked(goods[i])))
+            if (keccak256(abi.encodePacked(storeGoods)) == keccak256(abi.encodePacked(goods[i])))
             {
-                return _goods;
+                return "Product accepted!";
             }
         }
         return "Please enter from the products defined in the array.";
     }
 
-    function inputGoodsSize(string memory _newGoodsSize) public view returns (string memory)
+
+    function inputGoodsSize() public view returns (string memory)
     {
-        // // Checks whether the size of new product matches the product size  pre-defined in the goodsSize array.
+        // Checks whether the size of new product matches the product size  pre-defined in the goodsSize array.
         for (uint i = 0; i < goodsSize.length; i++)
         {
-            if (keccak256(abi.encodePacked(_newGoodsSize)) == keccak256(abi.encodePacked(goodsSize[i])))
+            if (keccak256(abi.encodePacked(storeGoodsSize)) == keccak256(abi.encodePacked(goodsSize[i])))
             {
-                return _newGoodsSize;
+                return "Product size accepted!";
             }
         }
         return "Please enter from the products defined in the array.";
     }
 
-    function inputGoodsQuality(string memory _newGoodsQuality) public view returns (string memory)
+    function inputGoodsQuality() public view returns (string memory)
     {
         // Checks whether the quality of the new product matches the product quality pre-defined in the goodsQuality array.
         for (uint i = 0; i < goodsQuality.length; i++)
         {
-            if (keccak256(abi.encodePacked(_newGoodsQuality)) == keccak256(abi.encodePacked(goodsQuality[i])))
+            if (keccak256(abi.encodePacked(storeGoodsQuality)) == keccak256(abi.encodePacked(goodsQuality[i])))
             {
-                return _newGoodsQuality;
+                return "Product quality accepted!";
             }
         }
         return "Please enter from the products defined in the array.";
